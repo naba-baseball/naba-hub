5f4f5a1f0d0707ad4c9a230ddc07a0f9a40d9f5e
Kaleb Ercanbrack <kaleb.ercanbrack@hey.com>
2022-12-30 00:01:24 -0700

    add changelog and change column

--- app.vue
+++ app.vue
@@ -3,9 +3,24 @@
     <Html data-theme="lofi" />
     <NuxtLoadingIndicator />
     <nav class="space-x-4 nav">
-      <NuxtLink class="hover:underline" active-class="underline font-bold" to="/">Home</NuxtLink>
-      <NuxtLink class="hover:underline" active-class="underline font-bold" to="/batting-stats/">Batting Stats</NuxtLink>
-      <!-- <NuxtLink class="hover:underline" active-class="underline font-bold" to="/changelog/ ">Changelog</NuxtLink> -->
+      <NuxtLink
+        class="hover:underline"
+        active-class="underline font-bold"
+        to="/"
+        >Home</NuxtLink
+      >
+      <NuxtLink
+        class="hover:underline"
+        active-class="underline font-bold"
+        to="/batting-stats/"
+        >Batting Stats</NuxtLink
+      >
+      <NuxtLink
+        class="hover:underline"
+        active-class="underline font-bold"
+        to="/changelog/"
+        >Changelog</NuxtLink
+      >
       <a href="https://github.com/Twitch0125/naba-hub">Github</a>
     </nav>
     <NuxtPage class="page" />
--- content/changelog.md
+++ content/changelog.md
@@ -1,3 +1,7 @@
+# Dec 30, 2022
+- Switched "Type" column to "Split"
+- Added Changelog page
+
 # Dec 29, 2022
 - Updated how most of the columns work
     - Mainly the number columns. Sorting on them was wonky, now it should be less wonky
