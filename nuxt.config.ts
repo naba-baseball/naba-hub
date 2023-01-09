7e33b98bd5b4e94f954a9a60a1a6c5188cfef3ba
Kaleb Ercanbrack <kaleb.ercanbrack@hey.com>
2023-01-09 00:04:58 -0700

    add card for fielding

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,6 +10,7 @@ export default defineNuxtConfig({
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
+      'tailwindcss/nesting': {}
     },
   },
   pwa:{
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,26 @@
 <template>
   <section class="flow">
     <h1 class="text-3xl font-bold">NABA Hub</h1>
-    <div class="card max-w-md items-center flow card-bordered">
-      <div class="card-title">Batting Stats</div>
-      <p class="card-body">Search all players across all years of the league</p>
-      <div class="card-actions">
-        <NuxtLink to="/batting-stats/" class="btn btn-primary">View</NuxtLink>
+    <div class="card-grid">
+      <div class="card max-w-md items-center flow card-bordered">
+        <div class="card-title">Batting Stats</div>
+        <p class="card-body">
+          Search batting stats for all players across all years of the league
+        </p>
+        <div class="card-actions">
+          <NuxtLink to="/batting-stats/" class="btn btn-primary">View</NuxtLink>
+        </div>
+      </div>
+      <div class="card max-w-md items-center flow card-bordered">
+        <div class="card-title">Fielding Stats</div>
+        <p class="card-body">
+          Search fielding stats all players across all years of the league
+        </p>
+        <div class="card-actions">
+          <NuxtLink to="/fielding-stats/" class="btn btn-primary"
+            >View</NuxtLink
+          >
+        </div>
       </div>
     </div>
   </section>
