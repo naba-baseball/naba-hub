d95b7598153d11573a71f8e14c638fa6ba20b256
Kaleb Ercanbrack <kaleb.ercanbrack@hey.com>
2023-02-21 20:48:18 -0700

    add split_id col

--- composables/usePitchingColumns.js
+++ composables/usePitchingColumns.js
@@ -7,7 +7,12 @@ const textHeaders = "lastname, firstname, team_name".split(
   },
   {},
 );
-
+const splitIdCol = {
+  headerName: "Split",
+  field: "split_id",
+  filter: "agTextColumnfilter",
+};
+textHeaders.split_id = splitIdCol;
 const numberHeaders =
   "year, g, gs, w, l, s, ip, ha, r, er, bb, hp, k, bf, ab, 1b, 2b, 3b, hr, tb, sh, sf, ci, iw, bk, wp, dp, qs, svopp, blownsv, reliefapp, cg, sho, holds, sb, cs, gb, fb, pitches, runsupport"
     .split(", ").reduce(
